a82299198e1e03b50465a761ce76dc289326c8d4
BonzerChien <BonzerChien@gmail.com>
2012-05-17 10:47:21 -0400

    Added comments.

--- src/com/hookmobile/age/Direction.java
+++ src/com/hookmobile/age/Direction.java
@@ -5,8 +5,17 @@ package com.hookmobile.age;
  */
 public enum Direction {
 	
+	/**
+	 * Finds contacts within your address book who has the same app.
+	 */
 	FORWARD,
+	/**
+	 * Finds other app users who has your phone number in their address book.
+	 */
 	BACKWARD,
+	/**
+	 * Finds contacts within your address book who has the same app and who also has your contact in his/her address book. 
+	 */
 	MUTUAL;
 	
 }
