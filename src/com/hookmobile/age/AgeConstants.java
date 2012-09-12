8ccf61cc1e9efe4ca4fafba91a91e8828a1ebf52
BonzerChien <BonzerChien@gmail.com>
2012-09-12 13:52:42 -0400

    1. Support Tapjoy UDID. 2. Optimize address book scanning.

--- src/com/hookmobile/age/AgeConstants.java
+++ src/com/hookmobile/age/AgeConstants.java
@@ -5,19 +5,19 @@ class AgeConstants {
 	static final String AGE_PREFERENCES         = "age_preferences";
 	static final String AGE_LOG                 = "Hook";
 	static final String AGE_CURRENT_APP_KEY     = "current_app_key";
-	static final String AGE_INSTALL_TOKEN		= "install_token";
+	static final String AGE_INSTALL_TOKEN       = "install_token";
 	static final String AGE_LAST_PHONE_COUNT    = "last_phone_count";
 	
 	static final String P_ADDRESSBOOK           = "addressBook";
-	static final String P_ADDRESS_HASH			= "addrHash";
+	static final String P_ADDRESS_HASH          = "addrHash";
 	static final String P_APP_KEY               = "appKey";
 	static final String P_DATE                  = "date";
-	static final String P_DESCRIPTION			= "desc";
+	static final String P_DESCRIPTION           = "desc";
 	static final String P_DEVICE_INFO           = "deviceInfo";
 	static final String P_INSTALL_CODE          = "installCode";
-	static final String P_INSTALL_TOKEN			= "installToken";
+	static final String P_INSTALL_TOKEN         = "installToken";
 	static final String P_LEADS                 = "leads";
-	static final String P_MAC_ADDRESS			= "macAddress";
+	static final String P_MAC_ADDRESS           = "macAddress";
 	static final String P_NAME                  = "name";
 	static final String P_OS_TYPE               = "osType";
 	static final String P_PHONE                 = "phone";
@@ -26,10 +26,10 @@ class AgeConstants {
 	static final String P_REFERRAL_TEMPLATE     = "referralTemplate";
 	static final String P_REFERRALS             = "referrals";
 	static final String P_REFERENCE             = "reference";
-	static final String P_SDK_VERSION			= "sdkVersion";
+	static final String P_SDK_VERSION           = "sdkVersion";
 	static final String P_SEND_NOW              = "sendNow";
-	static final String P_STATUS				= "status";
-	static final String P_TAPJOY_UDID			= "tapjoyUdid";
+	static final String P_STATUS                = "status";
+	static final String P_TAPJOY_UDID           = "tapjoyUdid";
 	static final String P_TOTAL_CLICK_THROUGH   = "totalClickThrough";
 	static final String P_TOTAL_INVITEE         = "totalInvitee";
 	static final String P_USE_VIRTUAL_NUMBER    = "useVirtualNumber";
@@ -37,7 +37,7 @@ class AgeConstants {
 	static final String P_VERIFY_MESSAGE        = "verifyMessage";
 	static final String P_VERIFY_MT             = "verifyMt";
 	
-	static final int E_SUCCESS					= 1000;
-	static final int E_DISCOVERY_EXPIRED		= 3502;
+	static final int E_SUCCESS                  = 1000;
+	static final int E_DISCOVERY_EXPIRED        = 3502;
 	
 }
